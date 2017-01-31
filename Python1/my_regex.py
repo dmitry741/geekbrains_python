1c0d1a95dc96275703960cdfef0aa13cf15f49e3
DMITRY PAVLOV <dmitrypavlov74@gmail.com>
2017-01-31 16:52:25 +0300

    parse email from text using regular expression
    commit on 31th jan 2017

--- Python1/my_regex.py
+++ Python1/my_regex.py
@@ -17,6 +17,26 @@ result = re.search(pattern2, string)
 print(result)
 
 # 4. Найти все цифры в тексте
-pattern3 = '\d+h'
+pattern3 = '[1-9]+'
 string = 'If 300 spartans were so brave, so 500 spartans efe34h1trh'
 print(re.findall(pattern3, string))
+
+# 5. Задание-1 из домашней работы урок 4
+# Вывести символы в нижнем регистре, которые окружают 1 или более символа в верхнем регистре.
+# Т.е. из строки "mtMmEZUOmcq" нужно получить ['mt', 'm', 'mcq']
+# Решить задачу двумя способами: с помощью re и без.
+line = 'mtMmEZUOmcqWiryMQhhTxqKdSTKCYEJlEZCsGAMkgAYEOmHBSQsSUHKvSfbmxULaysmNOGIPHpEMujalp'
+pattern5 = '[a-z]+'
+print(re.findall(pattern5, line))
+
+# 6. 
+pattern6 = r'[A-Z]{,2}\s'
+line = 'sGAMkgAYE OmHBSQsasd'
+print(line)
+print(re.findall(pattern6, line))
+      
+# 7. 
+pattern7 = r'\w+\.{,1}\w+@\w+\.\w{2,3}'
+line = 'dmitrypavlov74@gmail.com fewgfuwg few 321476gfwf \ndmitry.pavlov2012@yandex.ru ssuloev@gmail.com werwer'
+print('line = ', line)
+print(re.findall(pattern7, line))

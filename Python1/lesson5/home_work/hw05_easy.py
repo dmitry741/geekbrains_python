779ebe9e68869855703607e0d92474f3372cb643
dmitry741 <dmitrypavlov74@gmail.com>
2017-02-01 21:06:39 +0300

    Lesson5
    commit on 1 feb 2017

---
+++ Python1/lesson5/home_work/hw05_easy.py
@@ -0,0 +1,37 @@
+import os
+
+# Задача-1:
+# Напишите скрипт, создающий директории dir_1 - dir_9 в папке, из которой запущен данный скрипт.
+# И второй скрипт, удаляющий эти папки.
+
+# cоздаем директории
+# for p in range(1, 10):
+#     dir_path = os.path.join(os.getcwd(), 'dir_{}'.format(p))
+#     print(dir_path)
+#     try:
+#         os.mkdir(dir_path)
+#     except FileExistsError:
+#         print('Такая директория уже существует')
+#         break
+
+# удаляем директории
+# for p in range(1, 10):
+#     dir_path = os.path.join(os.getcwd(), 'dir_{}'.format(p))
+#     print(dir_path)
+#     try:
+#         os.rmdir(dir_path)
+#     except FileNotFoundError:
+#         print('Директория не существует')
+#         break
+
+# Задача-2:
+# Напишите скрипт, отображающий папки текущей директории.
+list_dir = os.listdir(os.getcwd())
+print(list_dir)
+
+for p in list_dir:
+    if os.path.isdir(p):
+        print(p)
+
+# Задача-3:
+# Напишите скрипт, создающий копию файла, из которого запущен данный скрипт.

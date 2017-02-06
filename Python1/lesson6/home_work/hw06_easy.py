999880a506fe6c1e77d292c185f1ef1460c30b9b
DMITRY PAVLOV <dmitrypavlov74@gmail.com>
2017-02-06 17:10:39 +0300

    home work 6 is completed
    ridan

--- Python1/lesson6/home_work/hw06_easy.py
+++ Python1/lesson6/home_work/hw06_easy.py
@@ -1,5 +1,3 @@
-# Задача-1: Написать класс для фигуры-треугольника, заданного координатами трех точек.
-# Определить методы, позволяющие вычислить: площадь, высоту и периметр фигуры.
 import math
 
 class my_point:
@@ -34,12 +32,32 @@ class my_geo_helper:
 
     @staticmethod
     def triangle_square(point1, point2, point3):
-        a = distance(point1, point2)
-        b = distance(point2, point3)
-        c = distance(point3, point1)
+        a = my_geo_helper.distance(point1, point2)
+        b = my_geo_helper.distance(point2, point3)
+        c = my_geo_helper.distance(point3, point1)
         p = (a + b + c) / 2
 
         return math.sqrt(p * (p - a) * (p - b) * (p - c))
+
+
+    @staticmethod
+    def is_collinear(point1, point2, point3, point4):
+        x1 = point1.GetX()
+        y1 = point1.GetY()
+        x2 = point2.GetX()
+        y2 = point2.GetY()
+
+        x3 = point3.GetX()
+        y3 = point3.GetY()
+        x4 = point4.GetX()
+        y4 = point4.GetY()
+        
+        ax = x2 - x1
+        ay = y2 - y1
+        bx = x4 - x3
+        by = y4 - y3
+
+        return (ax * by - bx * ay) == 0
         
 
 class my_base_figure:
@@ -55,7 +73,26 @@ class my_base_figure:
         return self.points[index]
 
     def Square(self):
-        pass
+        if len(self.points) < 3:
+            return 0
+
+        s = 0
+        index1, index2, index3 = 0, 1, 2
+        point1 = self.GetPoint(index1)
+        
+        while True:            
+            point2 = self.GetPoint(index2)
+            point3 = self.GetPoint(index3)
+
+            s += my_geo_helper.triangle_square(point1, point2, point3)
+
+            index2 += 1
+            index3 += 1
+
+            if index3 >= len(self.points):
+                break
+
+        return s        
 
     def Perimetr(self):
         d = 0
@@ -68,6 +105,10 @@ class my_base_figure:
             i += 1
 
         return d
+
+
+# Задача-1: Написать класс для фигуры-треугольника, заданного координатами трех точек.
+# Определить методы, позволяющие вычислить: площадь, высоту и периметр фигуры.
 
 class my_triangle(my_base_figure):
     ''' class my_triangle is based on my_base_figure. It is a triangle.
@@ -82,18 +123,35 @@ class my_triangle(my_base_figure):
     def to_string(self):
         print("I am a my_triangle)")
 
-point = my_point(1, 2)
-print(point.to_string())
-
-point1 = my_point(0, 0)
-point2 = my_point(1, 0)
-point3 = my_point(0, 1)
-
-tr = my_triangle(point1, point2, point3)
-print(tr.Perimetr())
-
 
 # Задача-2: Написать Класс "Равнобочная трапеция", заданной координатами 4-х точек.
 #  Предусмотреть в классе методы: проверка, является ли фигура равнобочной трапецией;
 #  вычисления: длины сторон, периметр, площадь.
+
+class my_isosceles_trapeze(my_base_figure):
+    ''' class my_isosceles_trapeze is based on my_base_figure. It is a isosceles trapeze.
+    '''
+    def __init__(self, point1, point2, point3, point4):
+        my_base_figure.__init__(self)
+        my_base_figure.AddPoint(self, point1)
+        my_base_figure.AddPoint(self, point2)
+        my_base_figure.AddPoint(self, point3)
+        my_base_figure.AddPoint(self, point4)
+
+    def is_correct(self):
+        if len(self.points) != 4:
+            return False
+
+        point1 = my_base_figure.GetPoint(0)
+        point2 = my_base_figure.GetPoint(1)
+        point3 = my_base_figure.GetPoint(2)
+        point4 = my_base_figure.GetPoint(3)
+
+        ls =[]
+        
+        ls.append(my_geo_helper.is_collinear(point1, point2, point3, point4))
+        ls.append(my_geo_helper.distance(point1, point2) == my_geo_helper.distance(point3, point4))
+
+        return all(ls)
+                  
 

af3aeb8425e8529c16939b28706965e71f495c30
DMITRY PAVLOV <dmitrypavlov74@gmail.com>
2017-02-06 12:16:59 +0300

    class project for geometrical figures
    commit from Ridan

--- Python1/lesson6/home_work/hw06_easy.py
+++ Python1/lesson6/home_work/hw06_easy.py
@@ -1,6 +1,8 @@
 # Задача-1: Написать класс для фигуры-треугольника, заданного координатами трех точек.
 # Определить методы, позволяющие вычислить: площадь, высоту и периметр фигуры.
 class my_point:
+    ''' class point X and Y
+    '''
     def __init__(self, x, y):
         self.x = x
         self.y = y
@@ -8,10 +10,30 @@ class my_point:
     def to_string(self):
         print("I am a point ({}, {})".format(self.x, self.y))
 
+class my_geo_helper:
+    ''' class my_geo_helper allows to calculate some
+    base properties for geometrical figures. 
+    '''
+    pass
+
+class my_base_figure:
+    ''' class my_base_figure is base for all geometrical figure
+    '''
+    def __init__(self):
+        self.points = []
+
+    def AddPoint(point):
+        self.points.append(point)
+
+    def GetPoint(index):
+        return self.points[index]        
+
 point = my_point(1, 2)
 print(point.to_string())
 
-class my_triangle:
+class my_triangle(my_base_figure):
+    ''' class my_triangle is based on my_base_figure
+    '''
     pass
 
 
